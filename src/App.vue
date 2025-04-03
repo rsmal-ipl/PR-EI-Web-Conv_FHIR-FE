a3b96b00493d7aa4c1a20e9cc857909cd170db0f
Pedro Silva <pedrogsilva04@gmail.com>
2025-04-03 23:41:49 +0100

    Add Autentication Forms

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,47 @@ import { RouterView } from 'vue-router'
 import { useTemplateRef, provide } from 'vue'
 import Toaster from '@/components/ui/toast/Toaster.vue'
 import GlobalAlertDialog from '@/components/common/GlobalAlertDialog.vue'
-import NavbarComponent from './components/navbarComponent.vue';
-import FooterComponent from './components/footerComponent.vue';
+import NavbarComponent from './components/navbarComponent.vue'
+import FooterComponent from './components/footerComponent.vue'
+import { ref, onMounted } from 'vue'
+import Button from '@/components/ui/button/Button.vue'
 
 const alertDialog = useTemplateRef('alert-dialog')
 provide('alertDialog', alertDialog)
+
+const isDarkMode = ref(false)
+
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value
+  document.documentElement.classList.toggle('dark', isDarkMode.value)
+  localStorage.setItem('darkMode', isDarkMode.value ? 'enabled' : 'disabled')
+}
+
+onMounted(() => {
+  const savedMode = localStorage.getItem('darkMode') ||  (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'enabled' : 'disabled')
+  isDarkMode.value = savedMode === 'enabled'
+  document.documentElement.classList.toggle('dark', isDarkMode.value)
+})
 </script>
 
 <template>
   <Toaster />
-  <GlobalAlertDialog ref="alert-dialog"></GlobalAlertDialog>
 
-  <div>
-    <NavbarComponent></NavbarComponent>
-    <main>
+  <GlobalAlertDialog />
+
+  <div class="min-h-screen flex flex-col">
+    <NavbarComponent />
+    
+    <main class="flex-1 bg-white dark:bg-gray-700 transition-colors duration-300">
       <RouterView />
     </main>
-    <FooterComponent></FooterComponent>
+    
+    <Button @click="toggleDarkMode"
+      class="fixed bottom-4 right-4 p-3 h-auto rounded-full bg-gray-200 dark:bg-gray-800 shadow-lg hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors">
+      {{ isDarkMode ? '🌙' : '☀️' }}
+    </Button>
+
+    <FooterComponent/>
   </div>
 
 </template>
-
-<style scoped>
-  div {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-  }
-</style>
--- src/components/navbarComponent.vue
+++ src/components/navbarComponent.vue
@@ -1,5 +1,38 @@
+<script setup>
+
+const middleLinks = [
+    { name: "Home", routeName: "home" },
+    { name: "About", routeName: "home" },
+    { name: "Convert", routeName: "home" },
+    { name: "Settings", routeName: "home" },
+];
+
+</script>
+
+
 <template>
-    <nav>
-        <h1>Navbar</h1>
+  <nav class="bg-gray-100 dark:bg-gray-800 dark:text-white p-4 flex items-center justify-between border-b-2">
+        <div class="flex items-center space-x-2">
+            <span class="text-2xl">⚙️</span>
+            <span class="text-lg font-semibold"
+                >JSON to FHIR</span
+            >
+        </div>
+
+        <div class="flex space-x-6">
+            <RouterLink :to="{ name: link.routeName }" 
+                v-for="link in middleLinks"
+
+                class="hover:text-gray-400 duration-300 transition">
+                {{ link.name }}
+            </RouterLink>
+        </div>
+
+        <div class="flex space-x-6">
+            <RouterLink :to="{ name: 'login' }"
+                class="hover:text-gray-400 duration-300 transition">
+                Login
+            </RouterLink>
+        </div>
     </nav>
 </template>
